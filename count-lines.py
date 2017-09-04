a2f55253d4aed72f17b4db919985e20c7af5e670
Shawn Axsom <shawn.axsom@ambyint.com>
2017-09-04 18:05:31 -0400

    Updated with beginner songs

--- count-lines.py
+++ count-lines.py
@@ -1,17 +1,21 @@
 import urllib
 
-songs = dict()
-file = open("./recommended-guitar-songs")
+def process_file(filename):
+    songs = dict()
+    # file = open("./recommended-guitar-songs")
+    file = open(filename)
 
-for line in file:
-    if line in songs:
-        songs[line] += 1
-    else:
-        songs[line] = 1
+    for line in file:
+        if line in songs:
+            songs[line] += 1
+        else:
+            songs[line] = 1
 
-out = open("./most-recommended-guitar-songs", "w")
+    out = open("most-" + filename, "w")
 
-for song, recommendation_count in sorted(songs.items(), key=lambda x: x[1], reverse=True):
-    line = "* {}: [{}](https://www.songsterr.com/a/wa/search?pattern={})\n".format(str(recommendation_count), song.replace('\n', ''), urllib.quote(song.replace("-", "")))
-    out.write(line)
+    for song, recommendation_count in sorted(songs.items(), key=lambda x: x[1], reverse=True):
+        line = "* {}: [{}](https://www.songsterr.com/a/wa/search?pattern={})\n".format(str(recommendation_count), song.replace('\n', ''), urllib.quote(song.replace("-", "")))
+        out.write(line)
 
+process_file("recommended-guitar-songs-beginner")
+process_file("recommended-guitar-songs-intermediate")
